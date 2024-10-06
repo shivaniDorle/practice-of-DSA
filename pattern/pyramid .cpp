374172e8f9b6a6d1d17ff17d58acd736d93045fe
Shivani Dorle <shivanidorle23@gmail.com>
2024-10-06 19:39:01 +0530

    function

--- pattern/pyramid .cpp
+++ pattern/pyramid .cpp
@@ -1,21 +1,28 @@
-#include <iostream>
+   #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    int n = 4;
 
-    int n=4;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n-i-1; j++){
-            cout<<" ";
+    for (int i = 0; i < n; i++) {
+        // Print spaces
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
         }
-        for(int j=1; j<=i+1; j++){
-            cout<<"j";
 
-            for(int j=1; j>0; j--){
-                cout<<j;
-            }
+        // Print increasing numbers
+        for (int j = 1; j <= i + 1; j++) {
+            cout << j;
         }
-        return 0;
+
+        // Print decreasing numbers
+        for (int j = i; j >= 1; j--) {
+            cout << j;
+        }
+
+        // Move to the next line
+        cout << endl;
     }
-}
 
+    return 0;
+}
